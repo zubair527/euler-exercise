3cb13557a18169315a2d602c5cf798e19fb33bcd
zubair <zubairm4@NETSOLPK.COM>
2019-08-08 13:22:42 +0500

    -work on streams

--- src/main/java/com/euler/practise/StreamsExercise.java
+++ src/main/java/com/euler/practise/StreamsExercise.java
@@ -10,8 +10,10 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.Spliterator;
 import java.util.function.Function;
 import java.util.function.UnaryOperator;
@@ -47,12 +49,44 @@ public class StreamsExercise {
         //Function<Integer, Integer> aFunc = (a) -> a+2;
         //System.out.println(aFunc.apply(3));
 
-        Stream<Integer> aStream = Stream.iterate(0, a->a+1).limit(2); // first it will print 0
+        Stream<Integer> aStream = Stream.iterate(0, a->a+1).skip(2).limit(5); // first it will print 0
         // then it will print 1. Mean that a+1 is evaluated after seeding the stream.
         Stream<Integer> bStream = Stream.iterate(0, a->aFunc.apply(a)  ).limit(2);
 
         aStream.forEach(a->System.out.println(a));
-        bStream.forEach(a->System.out.println(a));
+        //bStream.forEach(a->System.out.println(a));
+
+        // Below operation will fail bcz the stream has alredy been used.
+        //Stream<Integer> mergedStream = Stream.concat(aStream, bStream);
+
+
+        Stream<Integer> uniqueWords = Stream.of(1, 5, 7, 4, 4, 2).distinct();
+        //Integer res=uniqueWords.reduce(0, (a,b)->a+b);
+
+        uniqueWords.forEach(a->System.out.println(a));
+        uniqueWords.forEachOrdered(a->System.out.println(a));  // executes in order
+
+        Optional<Integer> highest = uniqueWords.peek(a-> System.out.println("Comapring: " +a)).sorted(Comparator.comparing(Integer::intValue).reversed()).findFirst();
+        //uniqueWords.sorted(Comparator.comparing(Integer::intValue).reversed()).findFirst();
+        System.out.println(highest.orElse(0));
+
+        Optional<String> someString = Stream.of("hellow", "world").parallel().filter(s-> s.startsWith("h")).findAny();
+        someString.ifPresent(s->System.out.println(s));
+
+        //someString.orElseGet(()->"hello");
+        //someString.orElse("hello");
+
+        /* ofNullable is possible bridge from null values to optional values.
+           Optional.ofNullable(obj) returns Optional.of(obj) if obj is not null and
+           Optional.empty() otherwise.
+        */
+        Optional<String> aString = Optional.ofNullable(null);
+
+        /*
+        s.f().g() can't work if f() and g() returns Optional
+        instead we need to use flatMap to flaten the nested value returned by f()
+         */
+        //Optional<U> flatenedResult = s.f().flatMap(T::g);
 
       }
 }
